266e73d807ce804d241603ddb7043d8d038ef8ba
15nisha <nishamadhav1998@gmail.com>
2021-12-04 17:51:27 +0530

    working file

--- api_job_roles.py
+++ api_job_roles.py
@@ -5,6 +5,7 @@ import numpy as np
 import re
 import io
 import joblib
+import pickle
 from nltk.corpus import stopwords
 import os
 from scipy.sparse import hstack
@@ -13,8 +14,9 @@ from sklearn.preprocessing import MultiLabelBinarizer
 #Load all important files to predict job_title
 #these files including encodings and pretrained models
 
-desc_title_vectorizer = '/home/harsh/job_roles_prediction_models/job_description_vectorizer'
-prediction_model = '/home/harsh/job_roles_prediction_models/svm_desc_title_prediction_model'
+desc_title_vectorizer = 'saved_model/vectorizer'
+prediction_model = 'saved_model/model'
+class_list = 'saved_model/classes.txt'
 
 #Datacleaning function to clean data
 def clean_data(job_data):
@@ -25,63 +27,56 @@ def clean_data(job_data):
     text = ' '.join([text for text in text.split() if text not in stop_words]) #removing stopwords
     return text
 
-def encoding_data_point(df):
-    encode_description = joblib.load(desc_title_vectorizer)
-    description_title_tfidf = encode_description.transform(df) # transform data point
-    return description_title_tfidf
 
-def find_class(df):
-    y = df['job_roles']
-    y = y.values.reshape(-1,1)
-    mlb = MultiLabelBinarizer()
-    y_bin = mlb.fit_transform(y)
-    return mlb
+# loaded_vectorizer = joblib.load(desc_title_vectorizer)
+# loaded_model = joblib.load(prediction_model)
+# loaded_class = pickle.load(open(class_list, 'rb'))
+# loaded_class = joblib.load(class_list)
 
-def infer_tags_svm(data):
-    q_vec = encoding_data_point(data)
-    print(q_vec)
-    q_pred = prediction_model.predict(q_vec)
-    return mlb.inverse_transform(q_pred)
 
-def main(df):
-    df['desc_title'] = df['description'] + ' '+ df['title']
-    df['clean_desc_title']= df['desc_title'].apply(clean_data)
+# def find_class(df):
+#     y = df['job_roles']
+#     y = y.values.reshape(-1,1)
+#     mlb = MultiLabelBinarizer()
+#     y_bin = mlb.fit_transform(y)
+#     return mlb
+
+def predict_job_roles(q):
+    q = clean_data(q)
+    q_vec = loaded_vectorizer.transform([q])
+    q_pred = loaded_model.predict(q_vec)
+    return loaded_class.inverse_transform(q_pred)
+
+def main(description , title):
+    final_string = description + ' '+ title
     
-    results = infer_tags_svm(df['clean_desc_title'])
-    print(results)
-
-    return 
+    result = predict_job_roles(final_string)
+    return result
 
 
-text1 = 'Must have excellent management and communication skills\r\n•\r\nFamiliarity with business skills as relating to ground operations, i.e. budgeting, cost control, labor allocation, and inventory management\r\n•\r\nProficient in MS Office suite'
-text2 = 'Airline - Ground Handling Manager'
-data = [[text1,text2]]
-col_names = ['description', 'title']
-df = pd.DataFrame(data,columns=col_names)
-
-main(df)
 
 
-# app=Flask(__name__)
+# text1 = 'Must have excellent management and communication skills\r\n•\r\nFamiliarity with business skills as relating to ground operations, i.e. budgeting, cost control, labor allocation, and inventory management\r\n•\r\nProficient in MS Office suite'
+# text2 = 'Airline - Ground Handling Manager'
+# # data = [[text1,text2]]
+# col_names = ['description', 'title']
+# df = pd.DataFrame(data,columns=col_names)
 
-# @app.route('/job_roles',methods= ["POST", "GET"] )
-# def job_role():
+# main(text1 ,text2)
 
-#     category = request.form.get('category')
-# def infer_tags(q):
-#     q = clean_data(q)
-#     q_vec = encode_description.transform([q])
-#     q_pred = model.predict(q_vec)
-#     # return mlb.inverse_transform(q_pred)#     title = request.form.get('title')
 
-# infer_tags(text2)
-#     predicted_job_roles = test(category, description, title)
+app=Flask(__name__)
 
-#     return jsonify(predicted_job_roles)
+@app.route('/job_roles',methods= ["POST", "GET"] )
+def job_role():
+    description = request.form.get('description')
+    title = request.form.get('title')
+    category = request.form.get('category')
+    job_roles = main(description, title)
+    return jsonify(job_roles)
 
-# if __name__=="__main__":
-#     # Load the model from disk
-#     # model = os.getenv("prediction_model")
-#     # loaded_model = joblib.load(model)
 
-#     app.run()
+
+if __name__=="__main__":
+
+    app.run()
