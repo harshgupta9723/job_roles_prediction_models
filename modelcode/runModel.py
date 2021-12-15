4c2be9a8d55abcb5cbcae62a6a61976cebd3c64d
varun-kumar-812 <varun.vk122@gmail.com>
2021-12-15 15:07:55 +0530

    model code for education data

--- modelcode/runModel.py
+++ modelcode/runModel.py
@@ -15,7 +15,7 @@ model = Model()
 def health(xtrain, xtest, ytrain, ytest, folder_name, category):
     print("################## Model building started #################\n")
     classifier = OneVsRestClassifier(estimator=XGBClassifier(gamma =0.2,max_depth = 4,min_child_weight=1,learning_rate=0.05,eval_metric='mlogloss',use_label_encoder =False))
-#                 classifier.fit(xtrain_tfidf, ytrain)
+    # classifier.fit(xtrain_tfidf, ytrain)
     # svc = SVC( kernel='rbf', C=1e9, gamma=1e-07)
     # svc = KNeighborsClassifier(n_neighbors = 5, weights = 'distance',algorithm = 'brute',metric = 'minkowski')
     # svm_clf = OneVsRestClassifier(svc)
@@ -66,7 +66,34 @@ def modelComputer():
     xtrain, xtest, ytrain, ytest , category = model.readAndProcessData("computer_and_it.csv", 
                                                                         "computer_and_it")
 
-    computer(xtrain, xtest, ytrain, ytest, category, category)    
+    computer(xtrain, xtest, ytrain, ytest, category, category)
 
 
-modelComputer()
+def education(xtrain, xtest, ytrain, ytest, folder_name, category):
+    
+    print("################## Model building started #################\n")
+    classifier = OneVsRestClassifier(estimator=SGDClassifier(eta0 = 1, loss = 'hinge', penalty = 'l1'))
+    classifier.fit(xtrain, ytrain)
+    print("################## Model building end #################\n")
+    # saving the model 
+    # make folder if not exist
+    
+    filename = f'models/{folder_name}/{category}.sav'
+    pickle.dump(classifier, open(f'models/{folder_name}/{category}.sav', 'wb'))
+    loaded_model = pickle.load(open(filename, 'rb'))
+
+    print("################## Making prediction #################\n")
+    sgd_pred = classifier.predict(xtest)
+    # evaluate performance
+    print(f1_score(ytest, sgd_pred, average="micro"))
+
+
+def modelEducation():
+    # preprocess text 
+    xtrain, xtest, ytrain, ytest , category = model.readAndProcessData("Education.csv", 
+                                                                        "education")
+
+    computer(xtrain, xtest, ytrain, ytest, category, category)        
+
+
+modelEducation()
