e89682e1a18c5cfacf3b0caaa6083bea8bab2cc0
15nisha <nishamadhav1998@gmail.com>
2021-12-16 17:32:23 +0530

    modified file

--- modelcode/runModel.py
+++ modelcode/runModel.py
@@ -11,13 +11,10 @@ from trainModel import Model
 
 model = Model()
 
-def health(xtrain, xtest, ytrain, ytest, folder_name, category):
+def cleaning_facility(xtrain, xtest, ytrain, ytest, folder_name, category):
     print("################## Model building started #################\n")
     classifier = KNeighborsClassifier(n_neighbors = 5, weights = 'distance',algorithm = 'brute',metric = 'minkowski')
-    # classifier = OneVsRestClassifier(estimator=XGBClassifier(gamma =0.2,max_depth = 4,min_child_weight=1,learning_rate=0.05,eval_metric='mlogloss',use_label_encoder =False))
-#                 classifier.fit(xtrain_tfidf, ytrain)
-    # rs = 5, weights = 'distance',algorithm = 'brute',metric = 'minkowski')
-    # svm_clf = OneVsRestClassifier(svc)
+
     classifier.fit(xtrain, ytrain)
     print("################## Model building end #################\n")
     # saving the model 
@@ -34,12 +31,12 @@ def health(xtrain, xtest, ytrain, ytest, folder_name, category):
         
 
 
-def modelHealth():
+def modelcleaning_facility():
     # preprocess text 
     xtrain, xtest, ytrain, ytest , category = model.readAndProcessData("cleaning_and_facilities.csv", 
                                                                         "cleaning_and_facilities")
 
-    health(xtrain, xtest, ytrain, ytest, category, category)
+    cleaning_facility(xtrain, xtest, ytrain, ytest, category, category)
 
 
-modelHealth()
+modelcleaning_facility()
