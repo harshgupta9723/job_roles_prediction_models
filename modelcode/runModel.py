7ef1cc0a3fcee24a2f23a5125c0bddf72c0b7c45
15nisha <nishamadhav1998@gmail.com>
2021-12-17 14:43:45 +0530

    restaurant

--- modelcode/runModel.py
+++ modelcode/runModel.py
@@ -1,4 +1,3 @@
-
 from sklearn.svm import SVC
 # Binary Relevance
 from sklearn.multiclass import OneVsRestClassifier
@@ -41,5 +40,34 @@ def modelHealth():
 
     health(xtrain, xtest, ytrain, ytest, category, category)
 
+def restaurant_and_hospitality(xtrain, xtest, ytrain, ytest, folder_name, category):
+    print("################## Model building started #################\n")
+    classifier = OneVsRestClassifier(estimator=XGBClassifier(gamma =0.0,max_depth = 4,min_child_weight=1,learning_rate=0.05,eval_metric='mlogloss',use_label_encoder =False))
+#                 classifier.fit(xtrain_tfidf, ytrain)
+    # svc = SVC( kernel='rbf', C=1e9, gamma=1e-07)
+    # svc = KNeighborsClassifier(n_neighbors = 5, weights = 'distance',algorithm = 'brute',metric = 'minkowski')
+    # svm_clf = OneVsRestClassifier(svc)
+    classifier.fit(xtrain, ytrain)
+    print("################## Model building end #################\n")
+    # saving the model 
+    # make folder if not exist
+    
+    filename = f'models/{folder_name}/{category}.sav'
+    pickle.dump(classifier, open(f'models/{folder_name}/{category}.sav', 'wb'))
+    loaded_model = pickle.load(open(filename, 'rb'))
 
-modelHealth()
+    print("################## Making prediction #################\n")
+    svm_pred = classifier.predict(xtest)
+    # evaluate performance
+    print(f1_score(ytest, svm_pred, average="micro"))
+        
+
+
+def modelrestaurant_and_hospitality():
+    # preprocess text 
+    xtrain, xtest, ytrain, ytest , category = model.readAndProcessData("Healthcare.csv", 
+                                                                        "healthcare")
+
+    restaurant_and_hospitality(xtrain, xtest, ytrain, ytest, category, category)
+
+modelrestaurant_and_hospitality
