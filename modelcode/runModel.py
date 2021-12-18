37af532c6075566d0afb1cc6ab31ead5c657eea8
ritikkumar01111999 <ritikkumar01111999@gmail.com>
2021-12-18 10:12:19 +0530

    ritik_models

--- modelcode/runModel.py
+++ modelcode/runModel.py
@@ -2,41 +2,42 @@
 from sklearn.svm import SVC
 # Binary Relevance
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 # Performance metric
 from sklearn.metrics import f1_score
 import pickle
-
+import xgboost as xgb
 from trainModel import Model
+from sklearn.linear_model import SGDClassifier
 
 model = Model()
 
-def health(xtrain, xtest, ytrain, ytest, folder_name, category):
+def advertising(xtrain, xtest, ytrain, ytest, folder_name, category):
     print("################## Model building started #################\n")
-    svc = SVC( kernel='rbf', C=1e9, gamma=1e-07)
-    # svc = KNeighborsClassifier(n_neighbors = 5, weights = 'distance',algorithm = 'brute',metric = 'minkowski')
-    svm_clf = OneVsRestClassifier(svc)
-    svm_clf.fit(xtrain, ytrain)
+    sgd_clf = OneVsRestClassifier(estimator=SGDClassifier(eta0 = 1, loss = 'hinge', penalty = 'l1'))
+    sgd_clf.fit(xtrain, ytrain)
     print("################## Model building end #################\n")
     # saving the model 
     # make folder if not exist
     
     filename = f'models/{folder_name}/{category}.sav'
-    pickle.dump(svm_clf, open(f'models/{folder_name}/{category}.sav', 'wb'))
+    pickle.dump(sgd_clf, open(f'models/{folder_name}/{category}.sav', 'wb'))
     loaded_model = pickle.load(open(filename, 'rb'))
 
     print("################## Making prediction #################\n")
-    svm_pred = svm_clf.predict(xtest)
+    svm_pred = sgd_clf.predict(xtest)
     # evaluate performance
     print(f1_score(ytest, svm_pred, average="micro"))
         
+def sales_and_retail(xtrain, xtest, ytrain, ytest, folder_name, category):
+    pass
 
-
-def modelHealth():
+def advertising_and_marketing():
     # preprocess text 
-    xtrain, xtest, ytrain, ytest , category = model.readAndProcessData("Healthcare.csv", 
-                                                                        "Healthcare")
+    xtrain, xtest, ytrain, ytest , category = model.readAndProcessData("advertising_and_marketing.csv", 
+                                                                        "advertising_and_marketing")
 
-    health(xtrain, xtest, ytrain, ytest, category, category)
+    advertising(xtrain, xtest, ytrain, ytest, category, category)
 
 
-modelHealth()
+advertising_and_marketing()
