ba6ccd00b61214571f00f5130b43c8318fbb924c
15nisha <nishamadhav1998@gmail.com>
2021-12-19 18:54:39 +0530

    modified file

--- modelcode/runModel.py
+++ modelcode/runModel.py
@@ -180,7 +180,8 @@ def model_cleaning_and_facilities():
     x,y,category = model.readAndProcessData("cleaning_and_facilities.csv","cleaning_and_facilities")
 
     cleaning_and_facilities(x,y, category, category)
-
+    
+modelEducation()
     
 def account_and_finance(x,y, folder_name, category):
     print("################## Model building started #################\n")
