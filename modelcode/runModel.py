a459b4c6897df97cdc5c357cdf9cd48d9986d014
15nisha <nishamadhav1998@gmail.com>
2021-12-31 15:17:58 +0530

    art model

--- modelcode/runModel.py
+++ modelcode/runModel.py
@@ -471,4 +471,22 @@ def model_animal_care():
 
     animal_care(x,y, category, category)
 
-model_animal_care()
+
+def art_fashion(x,y, folder_name, category):
+    print("################## Model building started #################\n")
+    sgd_clf = OneVsRestClassifier(estimator=SGDClassifier(loss = "hinge",alpha  = 0.001,penalty = "none"))
+    sgd_clf.fit(x,y)
+    print("################## Model building end #################\n")
+    # saving the model 
+    # make folder if not exist
+    #   
+    filename = f'models/{folder_name}/{category}.sav'
+    pickle.dump(sgd_clf, open(f'models/{folder_name}/{category}.sav', 'wb'))
+        
+def model_art_fashion():
+    # preprocess text 
+    x,y, category = model.readAndProcessData("Art, Fashion and Design.csv","art_fashion_and_design")
+
+    art_fashion(x,y, category, category)
+
+model_art_fashion()
